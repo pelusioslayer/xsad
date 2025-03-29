48ac607ab0031e28982582f0078fb81eada44ab7
qJulianx <99652819+qJulianx@users.noreply.github.com>
2025-03-29 22:39:28 +0100

    Update id.js

--- assets/id.js
+++ assets/id.js
@@ -57,7 +57,7 @@ function delay(time, length) {
     return new Promise(resolve => setTimeout(resolve, time));
 }
 
-eye.addEventListener('click', () => {
+
     var classlist = eye.classList;
     if (classlist.contains("eye_close")){
         classlist.remove("eye_close");
