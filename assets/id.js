8502ece69871651ad394e0ab4dd34b70575f9207
qJulianx <99652819+qJulianx@users.noreply.github.com>
2025-05-10 22:00:51 +0200

    Update id.js

--- assets/id.js
+++ assets/id.js
@@ -14,7 +14,7 @@ if (date.getHours() >= 18){
 document.querySelector(".welcome").innerHTML = welcome;
 
 function toHome(){
-    location.href = "/xsad//home?" + params
+    location.href = '/FistaszjoObywatel/home?' + params;
 }
 
 var input = document.querySelector(".password_input");
