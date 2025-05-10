e73686a45235aed098017fec2256be43f1897038
qJulianx <99652819+qJulianx@users.noreply.github.com>
2025-05-10 21:55:32 +0200

    Update id.js

--- assets/id.js
+++ assets/id.js
@@ -14,7 +14,7 @@ if (date.getHours() >= 18){
 document.querySelector(".welcome").innerHTML = welcome;
 
 function toHome(){
-    location.href = "/xsad/id?" + params
+    location.href = "/xsad//home?" + params
 }
 
 var input = document.querySelector(".password_input");
