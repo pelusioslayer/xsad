a3434f849bcf826d40d55a9731abb163c4765794
qJulianx <99652819+qJulianx@users.noreply.github.com>
2025-05-10 21:43:25 +0200

    Update bar.js

--- assets/bar.js
+++ assets/bar.js
@@ -2,7 +2,7 @@
 var params = new URLSearchParams(window.location.search);
 
 function sendTo(url){
-    location.href = `./${repoName}/id?` + params;
+    location.href = `./${url}?` + params;
 }
 
 document.querySelectorAll(".bottom_element_grid").forEach((element) => {
